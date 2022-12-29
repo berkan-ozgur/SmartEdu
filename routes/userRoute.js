63c6396ee6834c82dc33c4d9e68b991360c898d9
Berkan Özgür <berkanozgur@hotmail.com>
2022-12-29 21:01:46 +0300

    Dashboard page added.

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -6,4 +6,5 @@ const router = express.Router();
 router.route('/signup').post(authController.createUser); // http://localhost:3000/users/signup
 router.route('/login').post(authController.loginUser); // http://localhost:3000/users/login
 router.route('/logout').get(authController.logoutUser); // http://localhost:3000/users/logout
+router.route('/dashboard').get(authController.getDashboardPage); // http://localhost:3000/users/dashboard
 module.exports = router;
