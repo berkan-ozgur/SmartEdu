d3f45a09a757a38b6d6de73ebc0856d7fe99779c
Berkan Özgür <berkanozgur@hotmail.com>
2022-12-30 15:41:42 +0300

    Create a course function added.

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,5 @@
 const User = require('../models/User');
+const Category = require('../models/Category');
 const bcrypt = require('bcrypt');
 
 exports.createUser = async (req, res) => {
@@ -41,8 +42,10 @@ exports.logoutUser = async (req, res) => {
 
 exports.getDashboardPage = async (req, res) => {
     const user = await User.findOne({ _id: req.session.userID })
+    const categories = await Category.find();
     res.status(200).render('dashboard', {
         page_name: 'dashboard',
-        user
+        user,
+        categories
     });
 };
--- controllers/courseController.js
+++ controllers/courseController.js
@@ -4,10 +4,7 @@ const Category = require('../models/Category');
 exports.createCourse = async (req, res) => {
   try {
     const course = await Course.create(req.body);
-    res.status(201).json({
-      status: 'success',
-      course,
-    });
+    res.status(201).redirect('/courses');
   } catch (error) {
     res.status(400).json({
       status: 'fail',
@@ -27,7 +24,7 @@ exports.getAllCourses = async (req, res) => {
       filter = { category: category._id }
     }
 
-    const courses = await Course.find(filter);
+    const courses = await Course.find(filter).sort('-createdAt');
     const categories = await Category.find();
     res.status(200).render('courses', {
       courses,
