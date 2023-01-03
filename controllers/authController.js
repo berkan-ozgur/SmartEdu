a89ca69771ec4b1222aae45f061d0771b809177b
Berkan Özgür <berkanozgur@hotmail.com>
2023-01-03 21:14:26 +0300

    Admin page and User deletion methods added.

--- controllers/authController.js
+++ controllers/authController.js
@@ -50,10 +50,27 @@ exports.getDashboardPage = async (req, res) => {
     const user = await User.findOne({ _id: req.session.userID }).populate('courses')
     const categories = await Category.find();
     const courses = await Course.find({ user: req.session.userID })
+    const users = await User.find();
     res.status(200).render('dashboard', {
         page_name: 'dashboard',
         user,
         categories,
-        courses
+        courses,
+        users
     });
 };
+exports.deleteUser = async (req, res) => {
+    try {
+
+        await User.findByIdAndRemove(req.params.id)
+        await Course.deleteMany({ user: req.params.id })
+
+        res.status(200).redirect('/users/dashboard');
+
+    } catch (error) {
+        res.status(400).json({
+            status: 'fail',
+            error,
+        });
+    }
+}
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -24,4 +24,5 @@ router.route('/signup').post(
 router.route('/login').post(authController.loginUser); // http://localhost:3000/users/login
 router.route('/logout').get(authController.logoutUser); // http://localhost:3000/users/logout
 router.route('/dashboard').get(authMiddleware, authController.getDashboardPage); // http://localhost:3000/users/dashboard
+router.route('/:id').delete(authController.deleteUser);
 module.exports = router;
